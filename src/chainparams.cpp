b3196fd97244f328a4d0a94f782bf6b382334d3e
pasqualelembo78 <55604527+pasqualelembo78@users.noreply.github.com>
2025-04-10 18:10:13 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <chainparams.h>
 
 #include <chainparamsseeds.h>
@@ -7,12 +12,11 @@
 #include <util/system.h>
 #include <util/strencodings.h>
 #include <versionbitsinfo.h>
-#include "arith_uint256.h"
+
 #include <assert.h>
 
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
-
 
 static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
@@ -35,11 +39,21 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
     return genesis;
 }
 
-
+/**
+ * Build the genesis block. Note that the output of its generation
+ * transaction cannot be spent since it did not originally exist in the
+ * database.
+ *
+ * CBlock(hash=000000000019d6, ver=1, hashPrevBlock=00000000000000, hashMerkleRoot=4a5e1e, nTime=1231006505, nBits=1d00ffff, nNonce=2083236893, vtx=1)
+ *   CTransaction(hash=4a5e1e, ver=1, vin.size=1, vout.size=1, nLockTime=0)
+ *     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73)
+ *     CTxOut(nValue=50.00000000, scriptPubKey=0x5F1DF16B2B704C8A578D0B)
+ *   vMerkleTree: 4a5e1e
+ */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "Moneta digitale creata da Melissa e Vanessa in onore e ricordo di Desy";
-    const CScript genesisOutputScript = CScript() << ParseHex("045b212e02ad48ccd9f6af2349be7dd2eb1fb1620b752a5b2df21324593705e04906fe7f5fa2b7fc21b2eb6d5b5efd67e78c489d0cdb30a7b8e1c4406423886b66") << OP_CHECKSIG;
+    const char* pszTimestamp = "NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56";
+    const CScript genesisOutputScript = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
@@ -90,40 +104,37 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xda;  // distintivo per Mevacoin
-pchMessageStart[1] = 0xcd;  // M modificata
-pchMessageStart[2] = 0xd6;  // V modificata
-pchMessageStart[3] = 0xc3;  // C modificata
+        pchMessageStart[0] = 0xfb;
+        pchMessageStart[1] = 0xc0;
+        pchMessageStart[2] = 0xb6;
+        pchMessageStart[3] = 0xdb;
         nDefaultPort = 9333;
         nPruneAfterHeight = 100000;
         m_assumed_blockchain_size = 40;
         m_assumed_chain_state_size = 2;
-        genesis = CreateGenesisBlock(1710710401, 2218800825, 0x1f3fffff, 1, 50 * COIN);
-// MineGenesis(genesis, consensus.powLimit, true);
 
+        genesis = CreateGenesisBlock(1317972665, 2084524493, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000000f8c0b4460041bbc54b470b000d07cf1563edbe84598ade3a2bfef412"));
-        assert(genesis.hashMerkleRoot == uint256S("0x10385b293c52a8ae1d8ccba37a826ca0df7150e29eeb7d7e089402f6d30b6439"));
->>>>>>> 379890672d521a2ac0122515da783e5c45ba0394
-
+        assert(consensus.hashGenesisBlock == uint256S("0x12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2"));
+        assert(genesis.hashMerkleRoot == uint256S("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
 
         // Note that of those which support the service bits prefix, most only support a subset of
         // possible options.
         // This is fine at runtime as we'll fall back to using them as an addrfetch if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-       // vSeeds.emplace_back("seed-a.litecoin.loshan.co.uk");
-     //   vSeeds.emplace_back("dnsseed.thrasher.io");
-       // vSeeds.emplace_back("dnsseed.litecointools.com");
-      //  vSeeds.emplace_back("dnsseed.litecoinpool.org");
-      //  vSeeds.emplace_back("dnsseed.koin-project.com");
+        vSeeds.emplace_back("seed-a.litecoin.loshan.co.uk");
+        vSeeds.emplace_back("dnsseed.thrasher.io");
+        vSeeds.emplace_back("dnsseed.litecointools.com");
+        vSeeds.emplace_back("dnsseed.litecoinpool.org");
+        vSeeds.emplace_back("dnsseed.koin-project.com");
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);   // Può rimanere 48 per il PubKey Address
-base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,55);   // Modifica del Script Address
-base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,51);  // Modifica del secondo Script Address
-base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,190);   // Modifica della Secret Key
-base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB5, 0x1E};           // Modifica per il Public Key Extended
-base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xB0, 0xE4};           // Modifica per il Secret Key Extended
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
+        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,50);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
+        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
         bech32_hrp = "ltc";
         mweb_hrp = "ltcmweb";
@@ -137,16 +148,30 @@ base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xB0, 0xE4};           // Modifica
 
         checkpointData = {
             {
-                {},
-                
+                {  1500, uint256S("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967")},
+                {  4032, uint256S("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846")},
+                {  8064, uint256S("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70")},
+                { 16128, uint256S("0x602edf1859b7f9a6af809f1d9b0e6cb66fdc1d4d9dcd7a4bec03e12a1ccd153d")},
+                { 23420, uint256S("0xd80fdf9ca81afd0bd2b2a90ac3a9fe547da58f2530ec874e978fce0b5101b507")},
+                { 50000, uint256S("0x69dc37eb029b68f075a5012dcc0419c127672adb4f3a32882b2b3e71d07a20a6")},
+                { 80000, uint256S("0x4fcb7c02f676a300503f49c764a89955a8f920b46a8cbecb4867182ecdb2e90a")},
+                {120000, uint256S("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131")},
+                {161500, uint256S("0xdbe89880474f4bb4f75c227c77ba1cdc024991123b28b8418dbbf7798471ff43")},
+                {179620, uint256S("0x2ad9c65c990ac00426d18e446e0fd7be2ffa69e9a7dcb28358a50b2b78b9f709")},
+                {240000, uint256S("0x7140d1c4b4c2157ca217ee7636f24c9c73db39c4590c4e6eab2e3ea1555088aa")},
+                {383640, uint256S("0x2b6809f094a9215bafc65eb3f110a35127a34be94b7d0590a096c3f126c6f364")},
+                {409004, uint256S("0x487518d663d9f1fa08611d9395ad74d982b667fbdc0e77e9cf39b4f1355908a3")},
+                {456000, uint256S("0xbf34f71cc6366cd487930d06be22f897e34ca6a40501ac7d401be32456372004")},
+                {638902, uint256S("0x15238656e8ec63d28de29a8c75fcf3a5819afc953dcd9cc45cecc53baec74f38")},
+                {721000, uint256S("0x198a7b4de1df9478e2463bd99d75b714eab235a2e63e741641dc8a759a9840e5")},
             }
         };
 
         chainTxData = ChainTxData{
             // Data from rpc: getchaintxstats 17280 fdb81fc2edae4e315716890bd343d814184ea50331cd47166e19120a5163a678
-            /* nTime    */ 1710710401,
-            /* nTxCount */ 0,
-            /* dTxRate  */ 0.0,
+            /* nTime    */ 1728673449,
+            /* nTxCount */ 283070164,
+            /* dTxRate  */ 2.135848330748608
         };
     }
 };
@@ -193,26 +218,19 @@ public:
         consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000004260a1758f04aa");
         consensus.defaultAssumeValid = uint256S("0x4a280c0e150e3b74ebe19618e6394548c8a39d5549fd9941b9c431c73822fbd5"); // 1737876
 
-        pchMessageStart[0] = 0xda;  // distintivo per Mevacoin
-pchMessageStart[1] = 0xcd;  // M modificata
-pchMessageStart[2] = 0xd6;  // V modificata
-pchMessageStart[3] = 0xc3;  // C modificata
+        pchMessageStart[0] = 0xfd;
+        pchMessageStart[1] = 0xd2;
+        pchMessageStart[2] = 0xc8;
+        pchMessageStart[3] = 0xf1;
         nDefaultPort = 19335;
         nPruneAfterHeight = 1000;
         m_assumed_blockchain_size = 4;
         m_assumed_chain_state_size = 1;
 
-<<<<<<< HEAD
-        genesis = CreateGenesisBlock(1710710401, 1319, 0x1f3fffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1486949366, 293345, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x41ae2991ee0dfb00febc5cdc2bb5031e9fa4ca3bd2a84111676c7fb5a7d445c6"));
-        assert(genesis.hashMerkleRoot == uint256S("0x63f6d77b79b077f0b4309cf92144cc599a927e43f0889cddeaec044d608a6d79"));
-=======
-        genesis = CreateGenesisBlock(1742234454, 1074415, 0x1e0ffff0, 1, 50 * COIN);
-        consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000c99612f953c47a578abe225c228351f111638945235e252e78f7fab1a56"));
+        assert(consensus.hashGenesisBlock == uint256S("0x4966625a4b2851d9fdee139e56211a0d88575f59ed816ff5e6a63deb4e3e29a0"));
         assert(genesis.hashMerkleRoot == uint256S("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
->>>>>>> 379890672d521a2ac0122515da783e5c45ba0394
 
         vFixedSeeds.clear();
         vSeeds.clear();
@@ -221,12 +239,13 @@ pchMessageStart[3] = 0xc3;  // C modificata
         vSeeds.emplace_back("seed-b.litecoin.loshan.co.uk");
         vSeeds.emplace_back("dnsseed-testnet.thrasher.io");
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);   // Può rimanere 48 per il PubKey Address
-base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,55);   // Modifica del Script Address
-base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,51);  // Modifica del secondo Script Address
-base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,190);   // Modifica della Secret Key
-base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB5, 0x1E};           // Modifica per il Public Key Extended
-base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xB0, 0xE4};           // Modifica per il Secret Key Extended
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
+        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,58);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
+        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
+
         bech32_hrp = "tltc";
         mweb_hrp = "tmweb";
 
@@ -239,20 +258,17 @@ base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xB0, 0xE4};           // Modifica
 
         checkpointData = {
             {
-                {  0, uint256S("0x00000c99612f953c47a578abe225c228351f111638945235e252e78f7fab1a56")},
-                
+                {300, uint256S("54e6075affe658d6574e04c9245a7920ad94dc5af8f5b37fd9a094e317769740")},
+                {2056, uint256S("17748a31ba97afdc9a4f86837a39d287e3e7c7290a08a1d816c5969c78a83289")},
+                {2352616, uint256S("7540437e7bf7831fa872ba8cfae85951a1e5dbb04c201b6f5def934d9299f3c2")}
             }
         };
 
         chainTxData = ChainTxData{
-            // Data from rpc: getchaintxstats 17280 fdb81fc2edae4e315716890bd343d814184ea50331cd47166e19120a5163a678
-<<<<<<< HEAD
-            /* nTime    */ 1710710401,
-=======
-            /* nTime    */ 1742234454,
->>>>>>> 379890672d521a2ac0122515da783e5c45ba0394
-            /* nTxCount */ 0,
-            /* dTxRate  */ 0.0,
+            // Data from RPC: getchaintxstats 4096 36d8ad003bac090cf7bf4e24fbe1d319554c8933b9314188d6096ac12648764d
+            /* nTime    */ 1607986972,
+            /* nTxCount */ 4229067,
+            /* dTxRate  */ 0.06527021772939347,
         };
     }
 };
@@ -299,10 +315,10 @@ public:
         consensus.nMinimumChainWork = uint256{};
         consensus.defaultAssumeValid = uint256{};
 
-        pchMessageStart[0] = 0xda;  // distintivo per Mevacoin
-pchMessageStart[1] = 0xcd;  // M modificata
-pchMessageStart[2] = 0xd6;  // V modificata
-pchMessageStart[3] = 0xc3;  // C modificata
+        pchMessageStart[0] = 0xfa;
+        pchMessageStart[1] = 0xbf;
+        pchMessageStart[2] = 0xb5;
+        pchMessageStart[3] = 0xda;
         nDefaultPort = 19444;
         nPruneAfterHeight = 1000;
         m_assumed_blockchain_size = 0;
@@ -310,17 +326,10 @@ pchMessageStart[3] = 0xc3;  // C modificata
 
         UpdateActivationParametersFromArgs(args);
 
-<<<<<<< HEAD
-         genesis = CreateGenesisBlock(1710710401, 1319, 0x1f3fffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1296688602, 0, 0x207fffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x41ae2991ee0dfb00febc5cdc2bb5031e9fa4ca3bd2a84111676c7fb5a7d445c6"));
-        assert(genesis.hashMerkleRoot == uint256S("0x63f6d77b79b077f0b4309cf92144cc599a927e43f0889cddeaec044d608a6d79"));
-=======
-         genesis = CreateGenesisBlock(1742234454, 1074415, 0x1e0ffff0, 1, 50 * COIN);
-        consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000c99612f953c47a578abe225c228351f111638945235e252e78f7fab1a56"));
+        assert(consensus.hashGenesisBlock == uint256S("0x530827f38f93b43ed12af0b3ad25a288dc02ed74d6d7857862df51fc56c416f9"));
         assert(genesis.hashMerkleRoot == uint256S("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
->>>>>>> 379890672d521a2ac0122515da783e5c45ba0394
 
         vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();      //!< Regtest mode doesn't have any DNS seeds.
@@ -332,23 +341,22 @@ pchMessageStart[3] = 0xc3;  // C modificata
 
         checkpointData = {
             {
-                {  0, uint256S("0x00000c99612f953c47a578abe225c228351f111638945235e252e78f7fab1a56")},
-                
+                {0, uint256S("530827f38f93b43ed12af0b3ad25a288dc02ed74d6d7857862df51fc56c416f9")},
             }
         };
 
         chainTxData = ChainTxData{
-            1710710401,
+            0,
             0,
             0
         };
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);   // Può rimanere 48 per il PubKey Address
-base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,55);   // Modifica del Script Address
-base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,51);  // Modifica del secondo Script Address
-base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,190);   // Modifica della Secret Key
-base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB5, 0x1E};           // Modifica per il Public Key Extended
-base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xB0, 0xE4};           // Modifica per il Secret Key Extended
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
+        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,58);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
+        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
         bech32_hrp = "rltc";
         mweb_hrp = "tmweb";
